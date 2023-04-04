0cfdfd141d0e3dc04bb2df0f17e550dcf09f7e87
deepak7691 <kawliya11@gmail.com>
2023-04-04 14:53:04 +0530

    deepak

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -9,7 +9,23 @@ export default class Store extends Component {
   render() {
     return (
       <section>
-        
+        <ProductConsumer>
+          {value => {
+            const { cart } = value;
+            if (cart.length > 0) {
+              return (
+                <React.Fragment>
+                  <Title name="your" title="cart" />
+                  <CartColumns />
+                  <CartList value={value} />
+                  <CartTotals value={value} history={this.props.history} />
+                </React.Fragment>
+              );
+            } else {
+              return <EmptyCart />;
+            }
+          }}
+        </ProductConsumer>
       </section>
     );
   }
--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -11,7 +11,20 @@ export default class ProductList extends Component {
   render() {
     return (
       <React.Fragment>
-        START HACKING
+        <ProductWrapper className="py-5">
+          <div className="container">
+            <Title name="our" title="products" />
+            <div className="row">
+              <ProductConsumer>
+                {value => {
+                  return value.products.map(product => {
+                    return <Product key={product.id} product={product} />;
+                  });
+                }}
+              </ProductConsumer>
+            </div>
+          </div>
+        </ProductWrapper>
       </React.Fragment>
     );
   }
